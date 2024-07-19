2a0ed68ca1cb94184c954f11e2a6e54326ad3d13
Унгер Евгений Николаевич <unger.en@esphere.ru>
2024-07-19 15:16:58 +0500

    Change core library to Swiper.

--- src/app/ui/HalvesSwiper/ui/ImageSwiper.tsx
+++ src/app/ui/HalvesSwiper/ui/ImageSwiper.tsx
@@ -1,9 +1,7 @@
-import React, { useState, useRef } from 'react';
-import { useSwipeable, SwipeEventData } from 'react-swipeable';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import type { DefaultSwiperData } from '../types';
-
-import { LazyImage } from './LazyImage';
 
 /** Параметры компонентв */
 type Props<T extends DefaultSwiperData = DefaultSwiperData> = {
@@ -11,115 +9,27 @@ type Props<T extends DefaultSwiperData = DefaultSwiperData> = {
   images: T[];
 };
 
-/**
- * Компонент свайпа изображений
- * @returns - jsx.element
- */
 export const ImageSwiper = <T extends DefaultSwiperData>({
   images,
 }: Props<T>) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isSwiping, setIsSwiping] = useState(false);
-  const [swipeDelta, setSwipeDelta] = useState(0);
-  const [imageHeight, setImageHeight] = useState(0);
-
-  const swipeRef = useRef<HTMLDivElement>(null);
-
-  const handleSwiping = (event: SwipeEventData): void => {
-    setIsSwiping(true);
-    setSwipeDelta(event.deltaY);
-  };
-
-  const handleSwiped = (event: SwipeEventData): void => {
-    setIsSwiping(false);
-    setSwipeDelta(0);
-    const threshold = swipeRef.current?.offsetHeight || 0;
-
-    if (Math.abs(event.deltaY) > threshold / 2) {
-      if (event.deltaY < 0) {
-        setCurrentIndex((currentIndex + 1) % images.length);
-      } else {
-        setCurrentIndex((currentIndex - 1 + images.length) % images.length);
-      }
-    }
-  };
-
-  const { onMouseDown } = useSwipeable({
-    onSwiping: handleSwiping,
-    onSwiped: handleSwiped,
-
-    // onTap: console.log,
-    // onSwipeStart: console.log,
-    // onSwipedDown: console.log,
-    // onSwipedLeft: console.log,
-    // onSwipedUp: console.log,
-    // onSwipedRight: console.log,
-    // onTouchEndOrOnMouseUp: console.log,
-    // onTouchStartOrOnMouseDown: console.log,
-
-    // delta: 10, // Threshold for considering it a swipe
-    // swipeDuration: 500,
-    // preventScrollOnSwipe: true,
-    trackMouse: true,
-  });
-
-  const nextIndex = (currentIndex + 1) % images.length;
-  const prevIndex = (currentIndex - 1 + images.length) % images.length;
-
   return (
-    <div
-      className={`image-container ${isSwiping ? 'swiping' : ''}`}
-      style={{
-        height: `${imageHeight}px`,
-      }}
-      onMouseDown={onMouseDown}
-      ref={swipeRef}
+    <Swiper
+      lazy={true}
+      loop={true}
+      direction="vertical"
+      spaceBetween={16}
+      slidesPerView={1}
+      onSwiper={(swiper: any) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      style={{ height: '100%' }}
     >
-      <div
-        className="current-image"
-        style={{
-          transform: `translateY(${swipeDelta}px)`,
-          opacity: isSwiping ? 0.5 : 1,
-        }}
-      >
-        <img
-          src={images[currentIndex].image}
-          alt={`Image ${currentIndex}`}
-          onLoad={(event: React.SyntheticEvent<HTMLImageElement>) => {
-            setImageHeight((event.target as any)?.offsetHeight);
-          }}
-        />
-      </div>
-
-      {/* Следующее изображение (снизу) */}
-      {isSwiping && swipeDelta < -16 && (
-        <div
-          className="next-image"
-          style={{
-            transform: `translateY(${
-              swipeDelta + (swipeRef.current?.offsetHeight || 0) + 16
-            }px)`,
-            opacity: 0.5,
-          }}
-        >
-          <LazyImage src={images[nextIndex].image} alt={`Image ${nextIndex}`} />
-        </div>
-      )}
-
-      {/* Предыдущее изображение (сверху) */}
-      {isSwiping && swipeDelta > 16 && (
-        <div
-          className="prev-image"
-          style={{
-            transform: `translateY(${
-              swipeDelta - (swipeRef.current?.offsetHeight || 0) - 16
-            }px)`,
-            opacity: 0.5,
-          }}
-        >
-          <LazyImage src={images[prevIndex].image} alt={`Image ${prevIndex}`} />
-        </div>
-      )}
-    </div>
+      {images.map(({ image }, index) => (
+        <SwiperSlide key={index}>
+          <div className="slide-image">
+            <img src={image} alt={`Image ${index}`} loading="lazy" />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
